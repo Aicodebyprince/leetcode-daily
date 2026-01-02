e2d95be7857fafd22e66d2c8f56e5e7d479a9a06
Prince Sherathiya <126652751+Aicodebyprince@users.noreply.github.com>
2026-01-02 21:41:09 +0530

    Implement twoSum method in Solution class

--- Day-01-Two-Sum/solution.py
+++ Day-01-Two-Sum/solution.py
@@ -1,1 +1,11 @@
+class Solution:
+    def twoSum(self, nums, target):
+        data = {}
 
+        for i in range(len(nums)):
+            need = target - nums[i]
+
+            if need in data:
+                return [data[need], i]
+
+            data[nums[i]] = i
