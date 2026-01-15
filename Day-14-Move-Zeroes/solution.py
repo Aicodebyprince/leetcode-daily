3d01dea552733b146ae554133a2a72b05abad1cd
Prince Sherathiya <126652751+Aicodebyprince@users.noreply.github.com>
2026-01-15 21:26:16 +0530

    Add moveZeroes method to Solution class
    Implement moveZeroes method to shift zeros in-place.

--- Day-14-Move-Zeroes/solution.py
+++ Day-14-Move-Zeroes/solution.py
@@ -1,1 +1,10 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        k = 0
 
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[k] = nums[i]
+                if i != k:
+                    nums[i] = 0
+                k += 1
