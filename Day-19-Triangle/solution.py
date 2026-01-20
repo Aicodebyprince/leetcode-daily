fa0cb8240d14fc38173302ed684b2cd133b1b986
Prince Sherathiya <126652751+Aicodebyprince@users.noreply.github.com>
2026-01-20 21:26:49 +0530

    Add minimumTotal method for triangle path sum
    Implement dynamic programming solution to find minimum path sum in triangle.

--- Day-19-Triangle/solution.py
+++ Day-19-Triangle/solution.py
@@ -1,1 +1,11 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        # Copy the last row as initial DP
+        dp = triangle[-1][:]
 
+        # Start from second last row and move upward
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
+
+        return dp[0]
