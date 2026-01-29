cccc88fb3370f938e7d3a5367af35171c9cc20e5
Prince Sherathiya <126652751+Aicodebyprince@users.noreply.github.com>
2026-01-29 21:41:28 +0530

    Update solution.py

--- Day-28-Binary-Tree-Paths/solution.py
+++ Day-28-Binary-Tree-Paths/solution.py
@@ -1,1 +1,21 @@
+class Solution(object):
+    def binaryTreePaths(self, root):
+        result = []
 
+        def dfs(node, path):
+            if not node:
+                return
+
+            path += str(node.val)
+
+            # If leaf node, add path to result
+            if not node.left and not node.right:
+                result.append(path)
+                return
+
+            # Continue DFS
+            dfs(node.left, path + "->")
+            dfs(node.right, path + "->")
+
+        dfs(root, "")
+        return result
